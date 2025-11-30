6dcda3a6629ba2a859af8c3c6dde68297035bc07
rafaelGenish111 <rafaelgenish111@gmail.com>
2025-11-30 16:38:38 +0200

    fix White Screen error

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { CacheProvider } from '@emotion/react'
-import createCache from '@emotion/cache'
-import { prefixer } from 'stylis'
-import rtlPlugin from 'stylis-plugin-rtl'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
+import { prefixer } from 'stylis';
+import rtlPlugin from 'stylis-plugin-rtl';
+import { HelmetProvider } from 'react-helmet-async';
+import App from './App';
+import './index.css';
+import RtlProvider from './utils/RtlProvider';
 
-// פונקציה לאתחול בדיקות נגישות בסביבת פיתוח
+// בדיקת נגישות - רצה רק בסביבת פיתוח
 const initAxe = () => {
   if (import.meta.env.DEV) {
     import('@axe-core/react').then((axe) => {
       axe.default(React, ReactDOM, 1000);
-    });
+    }).catch(console.error);
   }
 };
 
@@ -26,9 +29,13 @@ const cacheRtl = createCache({
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <CacheProvider value={cacheRtl}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <HelmetProvider>
+        <BrowserRouter>
+          <RtlProvider>
+            <App />
+          </RtlProvider>
+        </BrowserRouter>
+      </HelmetProvider>
     </CacheProvider>
   </React.StrictMode>,
 )
