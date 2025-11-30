a52e28e60ac3b2574674cc61d6c7a4a81f6b993e
rafaelGenish111 <rafaelgenish111@gmail.com>
2025-11-30 16:51:03 +0200

    fix some issues

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { CacheProvider } from '@emotion/react';
-import createCache from '@emotion/cache';
-import { prefixer } from 'stylis';
-import rtlPlugin from 'stylis-plugin-rtl';
 import { HelmetProvider } from 'react-helmet-async';
 import App from './App';
 import './index.css';
-import RtlProvider from './utils/RtlProvider';
+import { RtlProvider } from './utils/RtlProvider';
 
 // בדיקת נגישות - רצה רק בסביבת פיתוח
 const initAxe = () => {
@@ -21,21 +17,14 @@ const initAxe = () => {
 
 initAxe();
 
-const cacheRtl = createCache({
-  key: 'muirtl',
-  stylisPlugins: [prefixer, rtlPlugin],
-});
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CacheProvider value={cacheRtl}>
-      <HelmetProvider>
-        <BrowserRouter>
-          <RtlProvider>
-            <App />
-          </RtlProvider>
-        </BrowserRouter>
-      </HelmetProvider>
-    </CacheProvider>
+    <HelmetProvider>
+      <BrowserRouter>
+        <RtlProvider>
+          <App />
+        </RtlProvider>
+      </BrowserRouter>
+    </HelmetProvider>
   </React.StrictMode>,
 )
